919ee8d1cbba85251a16c27da6f80a2871acd3c6
FlorianGalvani <florian.galvani@gmail.com>
2020-12-07 09:36:12 +0100

    Premier

---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+  <footer>
+
+    <div class="social_network">
+
+    </div>
+
+  </footer>
+</body>
+<script src="asset/js/jquery.js"></script>
+<script src="asset/js/jquery.flexslider.js"></script>
+<script src="asset/js/script.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include('inc/pdo.php'); 
+
+include('inc/function.php');
+
+$title = 'Home' ;
+$page = 'index.php';
+
+include('inc/header.php'); ?>
+
